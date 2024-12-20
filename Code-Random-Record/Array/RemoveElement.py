1b52cbb729d09585ff504c1e99a5b4706fe54a4b
dev1ce <1161297340@qq.com>
2024-12-20 17:20:44 +0800

    20241220

---
+++ Code-Random-Record/Array/RemoveElement.py
@@ -0,0 +1,3 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        return -1
---
+++ Code-Random-Record/Array/test.py
@@ -0,0 +1,5 @@
+# test function
+import BinarySearch
+if __name__ == '__main__':
+    # BinarySearch
+    print(BinarySearch.Solution.search(None, nums=[-1, 0, 3, 5, 9, 12], target=9))
