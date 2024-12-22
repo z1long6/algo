a361f4c8abd149c1d26bf6184961c7db55e22abe
dev1ce <1161297340@qq.com>
2024-12-22 18:46:04 +0800

    20241222 coding

--- Code-Random-Record/Array/BinarySearch.py
+++ Code-Random-Record/Array/BinarySearch.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 二分查找
     def search(self, nums: list[int], target: int) -> int:
         left, right = 0, len(nums)-1
         while(left <= right):
@@ -10,3 +11,28 @@ class Solution:
             else:
                 left = mid+1
         return -1
+    
+    # LT.35.搜索插入位置
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        # binary search
+        left, right = 0, len(nums)-1
+        while(left <= right):
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid # find target
+            elif nums[mid] > target:
+                right = mid-1
+            else:
+                left = mid+1
+
+        # do not find target
+        if left == len(nums):
+            return len(nums)
+        elif right == -1:
+            return 0
+        else:
+            return left
+        
+    # LT.34.在排序数组中查找元素的第一个和最后一个位置
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        return 0 
--- Code-Random-Record/Array/test.py
+++ Code-Random-Record/Array/test.py
@@ -10,11 +10,16 @@ if __name__ == '__main__':
     target = 7
     # BinarySearch
     # print(BinarySearch.Solution.search(None, nums=[-1, 0, 3, 5, 9, 12], target=9))
+    print(BinarySearch.Solution.searchInsert(None, [1,3,5,6], 2))
+
     # RemoveElement
     # print(RemoveElement.Solution.removeElement3(None, nums, val))
+
     # SquareOrderedArray
     # print(SquareOrderedArray.Solution.sortedSquares2(None, [-7,-3,2,3,11]))
+
     # Subarray with smallest length
     # print(SubarrayWithSmallestLength.Solution.minSubArrayLen2(None, target, nums))
+
     # Spiral Matrix
-    print(SpiralMartix.Solution.generateMatrix(None, 3))
+    # print(SpiralMartix.Solution.generateMatrix(None, 3))
