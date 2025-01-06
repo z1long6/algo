74692c29139f175d1297368ac235250354e8b405
dev1ce <1161297340@qq.com>
2025-01-06 09:42:47 +0800

    finish My LinkedList

---
+++ Code-Random-Record/LinkedList/MyLinkList.py
@@ -0,0 +1,49 @@
+from MyListNode import ListNode
+
+class MyLinkedList:
+    '''
+        my linkedlist
+    '''
+    def __init__(self):
+        self.dummy_head = ListNode() # dummy head node
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        
+        current = self.dummy_head.next
+        for _ in range(index):
+            current = current.next
+        return current.val
+
+    def addAtHead(self, val: int) -> None:
+        self.dummy_head.next = ListNode(val, self.dummy_head.next)
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        current = self.dummy_head
+        while current.next:
+            current = current.next
+        current.next = ListNode(val)
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        
+        current = self.dummy_head
+        for _ in range(index):
+            current = current.next
+        current.next = ListNode(val, current.next)
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        
+        current = self.dummy_head
+        for _ in range(index):
+            current = current.next
+        current.next = current.next.next
+        self.size -= 1
--- Code-Random-Record/LinkedList/MyListNode.py
+++ Code-Random-Record/LinkedList/MyListNode.py
@@ -5,4 +5,3 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
