cc1ca59ffc904c7f8a4f9450810bbd1d75cf9a44
dev1ce <1161297340@qq.com>
2025-03-07 20:39:09 +0800

    LT.20

--- Code-Random-Record/StackandQueue/StackBase.py
+++ Code-Random-Record/StackandQueue/StackBase.py
@@ -1,4 +1,19 @@
 class Solution:
     # LT.20.有效的括号
     def isValid(self, s: str) -> bool:
-        pass
+        list = []
+        for i, item in enumerate(s):
+            if item in ['(', '[', '{']:
+                list.append(item)
+            elif item == ')':
+                if len(list) == 0 or list.pop() != '(':
+                    return False   
+            elif item == ']':
+                if len(list) == 0 or list.pop() != '[':
+                    return False  
+            elif item == '}':
+                if len(list) == 0 or list.pop() != '{':
+                    return False
+        if len(list) != 0:
+            return False  
+        return True
