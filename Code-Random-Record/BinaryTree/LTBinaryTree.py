a97b39b1fd94786995665ebf8c6912b04843c1f9
dev1ce <1161297340@qq.com>
2025-03-15 14:58:49 +0800

    finish LT.515

--- Code-Random-Record/BinaryTree/LTBinaryTree.py
+++ Code-Random-Record/BinaryTree/LTBinaryTree.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from TreeNode import TreeNode
 from collections import deque
 class LTSolution:
@@ -40,3 +41,25 @@ class LTSolution:
         
         dfs(root, 1)
         return res
+    
+    # LT.515.在每个树行中找最大值
+    def largestValues(self, root: Optional[TreeNode]) -> list[int]:
+        if not root:
+            return []
+        queue_ = deque()
+        res = []
+        queue_.append(root)
+        while queue_:
+            level = []
+            for _ in range(len(queue_)):
+                node = queue_.popleft()
+                if node.left:
+                    queue_.append(node.left)
+                if node.right:
+                    queue_.append(node.right)
+                level.append(node.val)
+            res.append(level)
+        res2 = []
+        for index, _ in enumerate(res):
+            res2.append(max(_))
+        return res2
