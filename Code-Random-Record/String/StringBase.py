bdee3e8ded9120ab0139df734359ff85b4046d15
dev1ce <1161297340@qq.com>
2025-03-17 10:29:02 +0800

    finish LT.239

--- Code-Random-Record/String/StringBase.py
+++ Code-Random-Record/String/StringBase.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     # LT.344.反转字符串
     def reverseString(self, s: list[str]) -> None:
@@ -21,3 +22,22 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         # math
         return s in (s + s)[1:-1]    
+    
+    # LT.239.滑动窗口最大值
+    # 滑动窗口框架
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        res = []
+        queue_ = deque()
+        for i, x in enumerate(nums):
+            # 1.元素进入窗口
+            while queue_ and nums[queue_[-1]] <= x:
+                queue_.pop()
+            queue_.append(i)
+            # 2.元素离开窗口
+            if i-queue_[0] >= k:
+                queue_.popleft()
+
+            # 3.记录答案
+            if i >= k-1:
+                res.append(nums[queue_[0]])
+        return res
